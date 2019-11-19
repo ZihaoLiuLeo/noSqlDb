91d0abd1b6254e92b209ad1f104c42f2790522ed
ZihaoLiuLeo <zihaoliu375@gmail.com>
2019-11-19 00:22:51 -0500

    checkin. question remained in comments

--- CheckIn/CheckIn.h
+++ CheckIn/CheckIn.h
@@ -7,31 +7,94 @@
 
 
 #include <string>
+#include "../PayLoad/Payload.h"
 #include "../RepositoryCore/RepositoryCore.h"
 
-template<typename P>
-class CheckIn
-{
-	using FileName = std::string;
-	using PackageName = std::string;
-	using Status = bool;
+namespace RepoCore {
+	template<typename P>
+	class CheckIn
+	{
+		using FileName = std::string;
+		using PackageName = std::string;
+		using Status = bool;
 
-public:
-	CheckIn() = default;
-	CheckIn<P>(RepositoryCore<P>& rc) : rc_(rc) {};
-	CheckIn(const CheckIn<P>& ci);
-	CheckIn(CheckIn<P>&& ci);
+	public:
+		CheckIn() = default;
+		CheckIn<P>(RepositoryCore<P>& rc) : rc_(rc) {};
+		CheckIn(const CheckIn<P>& ci);
+		CheckIn(CheckIn<P>&& ci);
 
-	CheckIn<P>& operator=(const CheckIn<P>& ci);
-	CheckIn<P>& operator=(CheckIn<P>&& ci);
+		CheckIn<P>& operator=(const CheckIn<P>& ci);
+		CheckIn<P>& operator=(CheckIn<P>&& ci);
 
-	CheckIn<P>& addFile(FileName fn); // open checkin then store info in db, store file in repo, add version to filename, close then update version
-	CheckIn<P>& addPackage(PackageName pn);
-	
-	CheckIn<P>& setStatus(Status isOpen) { isOpen = isOpen; }
+		CheckIn<P>& addFile(P p); // open checkin then store info in db, store file in repo, add version to filename, close then update version
+		CheckIn<P>& addPackage(PackageName pn);
 
-	bool ableToSetStatus(); // fidn all dependent file for this check in
-private:
-	RepositoryCore<P>& rc_;
-	Status isOpen = true;
-};
+		CheckIn<P>& setStatus(Status status)
+
+		bool ableToSetStatus(P p); // find all dependent file for this check in
+
+		P* payload() { return p; }
+		P payload() const { return p; }
+		void payload(const P& payload) { p = payload; }
+
+	private:
+		RepositoryCore<P>& rc_;
+		P p;
+		Status isOpen = true;
+	};
+
+	template<typename P>
+	CheckIn<P>::CheckIn(const CheckIn<P>& ci)
+	{
+		rc_ = ci.rc_;
+	}
+
+	template<typename P>
+	CheckIn<P>::CheckIn(CheckIn<P>&& ci)
+	{
+		rc_ = ci.rc_;
+	}
+
+	template<typename P>
+	CheckIn<P>& CheckIn<P>::operator=(const CheckIn<P>& ci)
+	{
+		if (this == &ci) return *this;
+		rc_ = ci.rc_;
+		return *this;
+	}
+
+	template<typename P>
+	CheckIn<P>& CheckIn<P>::operator=(CheckIn<P>&& ci)
+	{
+		if (this == &ci) return *this;
+		rc_ = ci.rc_;
+		return *this;
+	}
+
+	template<typename P>
+	CheckIn<P>& CheckIn<P>::addFile(P p)
+	{
+		if (isOpen)
+		{
+			rc_.store(p);
+			//TODO denpendency check
+			/*
+			open p.value();
+			new filename: filename + version.num(), copy content
+			add to rc_.path();
+			*/
+		}
+		return *this;
+	}
+
+	template<typename P>
+	bool CheckIn<P>::ableToSetStatus()
+	{
+		/*
+		for d in p.depent
+			find d in rc_[p.value].children()
+		*/
+		return true
+	}
+}
--- RepositoryCore/RepositoryCore.h
+++ RepositoryCore/RepositoryCore.h
@@ -8,18 +8,79 @@
 #include "../DbCore/DbCore.h"
 #include "../Query/Query.h"
 #include "../PayLoad/Payload.h"
+#include <string>
+
 
 namespace RepoCore
 {
 	using namespace NoSqlDb;
-
-	template<P>
+	
+	template<typename P>
 	class RepositoryCore
 	{
 	public:
+		using FilePath = std::string;
+		using Key = std::string;
+
+		RepositoryCore() = default;
+		RepositoryCore(const RepositoryCore<P>& rc);
+		RepositoryCore(RepositoryCore<P>&& rc);
+
+		RepositoryCore<P>& operator=(const RepositoryCore<P>& q);
+		RepositoryCore<P>& operator=(RepositoryCore<P> && q);
+
+		FilePath& path() { return path; }
+		FilePath path() const { return path; }
+		void path(FilePath& path) { path = path };
+
+		RepositoryCore<P>& store(P p);
+
 	private:
-		DbCore<P>* p_Db;
+		DbCore<P> db_;
+		FilePath path = "";
+		//DbElement<P>* p_dbe = nullptr;
 	};
+
+	//----< copy constructor >--------------------------------------------
+	template<typename P>
+	RepositoryCore<T>::RepositoryCore(const RepositoryCore<T>& rc)
+	{
+		p_db = rc.p_db;
+	}
+
+	//----< move constructor >--------------------------------------------
+	template<typename P>
+	RepositoryCore<T>::RepositoryCore(RepositoryCore<T>&& rc)
+	{
+		p_db = rc.p_db;
+	}
+
+	//----< copy assignment >--------------------------------------------
+	template<typename P>
+	RepositoryCore<T>& RepositoryCore<T>::operator=(const RepositoryCore<T>& rc)
+	{
+		if (this == &q) return *this;
+		p_db = rc.p_db;
+		return *this;
+	}
+
+	//----< move assignment >--------------------------------------------
+	template<typename P>
+	RepositoryCore<T>& RepositoryCore<T>::operator=(RepositoryCore<T>&& rc)
+	{
+		if (this == &q) return *this;
+		p_db = rc.p_db;
+		return *this;
+	}
+
+	template<typename P>
+	RepositoryCore<P>& RepositoryCore<P>::store(P p)
+	{
+		Key key = p.value;
+		DbElement<P> dbe = db_[key];
+		dbe.payLoad = p;
+		return *this;
+	}
 
 	/*
 	PayLoad pl;
